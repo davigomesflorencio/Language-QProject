a2eb291fab390253057d3818a29f6d6cbe55957b
davigomesflorencio <davigomesflorencio@gmail.com>
2019-06-10 14:24:38 -0300

    Semantica Atribuição e Operadores Binários

--- src/main/java/br/ufc/qproject/frontend/SemanticaChecker.java
+++ src/main/java/br/ufc/qproject/frontend/SemanticaChecker.java
@@ -26,15 +26,41 @@ public class SemanticaChecker extends qprojectBaseListener {
     @Override
     public void enterTipo(qprojectParser.TipoContext ctx) {
         if(!(ctx.qualificador().isEmpty() || ctx.decorador().isEmpty())) {
-            //ERRO Semantico
+            // TODO ERRO Semantico
         }
     }
 
     @Override
     public void enterVariavel(qprojectParser.VariavelContext ctx) {
-        Symbol s = new Symbol(ctx.tipo().tipo_nome().ID().toString());
-        if(symbols.isDeclared(s)){
-            System.out.println("ERRO SEMANTICO");
+         Symbol s = Symbol.getSymbolFor(ctx.tipo().tipo_nome().ID().getText());
+         SymbolType t = SymbolType.fromString(ctx.tipo().getText());
+        if (!symbols.tryDeclare(s, t)) {
+            // TODO ERRO SEMANTICO
+        }
+    }
+
+    @Override
+    public void exitExpressionOperatorBinary(qprojectParser.ExpressionOperatorBinaryContext ctx) {
+        if (expressionType.get(ctx.expressao(0)).TypeIsBoolean() &&
+                expressionType.get(ctx.expressao(1)).TypeIsBoolean() &&
+                ctx.operador_binario().OR().getText().equals("||")){
+            expressionType.put(ctx,SymbolType.BOOLEAN);
+        }else{
+            expressionType.put(ctx,SymbolType.INVALID);
+        }
+    }
+
+    @Override
+    public void exitAtribuicao(qprojectParser.AtribuicaoContext ctx) {
+        Symbol s = Symbol.getSymbolFor(ctx.nome().ID().getText());
+        if(symbols.getType(s).get().TypeIs_BOOLEAN_QUAL()
+                || symbols.getType(s).get().TypeIs_BOOLEAN_QUAL_DECOR()
+                || symbols.getType(s).get().TypeIs_INTL_QUAL()
+                || symbols.getType(s).get().TypeIs_INTL_QUAL_DECOR()
+                || symbols.getType(s).get().TypeIs_CHARL_QUAL()
+                || symbols.getType(s).get().TypeIs_CHARL_QUAL_DECOR()
+        ){
+            //TODO ERRO SEMANTICO
         }
     }
 }
